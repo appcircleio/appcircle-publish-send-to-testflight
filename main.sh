458bf00b92d8b4411f805d6c0fd35ca3d7253bf3
Enver GÖKMEN <envergokmen@gmail.com>
2024-07-24 13:23:24 +0300

    BE-4093 env name convention applied

--- main.sh
+++ main.sh
@@ -4,20 +4,20 @@
       export LANG=en_US.UTF-8
       export LANGUAGE=en_US.UTF-8
 
-      echo "IPAFileName:$IPAFileName"
-      echo "IPAFileUrl:$IPAFileUrl"
-      echo "AppleId:$AppleId"
-      echo "BundleId:$BundleId"
-      echo "AppleUserName:$AppleUserName"
-      echo "ApplicationSpecificPassword:$ApplicationSpecificPassword"
-      echo "AppStoreConnectApiKey:$AppStoreConnectApiKey"
-      echo "AppStoreConnectApiKeyFileName:$AppStoreConnectApiKeyFileName"
-      echo "appleStoreSubmitApiType:$appleStoreSubmitApiType"
+      echo "IPAFileName:$AC_APP_FILE_NAME"
+      echo "IPAFileUrl:$AC_APP_FILE_URL"
+      echo "AppleId:$AC_APPLE_ID"
+      echo "BundleId:$AC_BUNDLE_ID"
+      echo "AppleUserName:$AC_APPLE_APP_SPECIFIC_USERNAME"
+      echo "ApplicationSpecificPassword:$AC_APPLE_APP_SPECIFIC_PASSWORD"
+      echo "AppStoreConnectApiKey:$AC_API_KEY"
+      echo "AppStoreConnectApiKeyFileName:$AC_API_KEY_FILE_NAME"
+      echo "appleStoreSubmitApiType:$AC_APPLE_STORE_SUBMIT_API_TYPE"
       
       locale
-      curl -o "./$IPAFileName" -k $IPAFileUrl
+      curl -o "./$AC_APP_FILE_NAME" -k $AC_APP_FILE_URL
        
-     if [ "$appleStoreSubmitApiType" == 1 ] || [ "$appleStoreSubmitApiType" == "AppStoreConnectApiConnection" ]; then
+     if [ "$AC_APPLE_STORE_SUBMIT_API_TYPE" == 1 ] || [ "$AC_APPLE_STORE_SUBMIT_API_TYPE" == "AppStoreConnectApiConnection" ]; then
  
         bundle init
         echo "gem \"fastlane\"">>Gemfile
@@ -25,10 +25,10 @@
         mkdir fastlane
         touch fastlane/Appfile
         touch fastlane/Fastfile
-        mv $FastFileConfig "fastlane/Fastfile"
-        mv "$AppStoreConnectApiKey" "$AppStoreConnectApiKeyFileName"
+        mv $AC_FASTFILE_CONFIG "fastlane/Fastfile"
+        mv "$AC_API_KEY" "$AC_API_KEY_FILE_NAME"
 
-        if [ $StackType == 10 ]
+        if [ $AC_STACK_TYPE == 10 ]
         then
           bundle exec fastlane doTestFlight --verbose
           if [ $? -eq 0 ]
@@ -40,7 +40,7 @@
             exit 1
           fi
         fi
-        if [ $StackType == 12 ]
+        if [ $AC_STACK_TYPE == 12 ]
         then
           bundle exec fastlane doRelease --verbose
           if [ $? -eq 0 ] 
